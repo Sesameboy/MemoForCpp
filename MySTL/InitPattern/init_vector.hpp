0fb7c0610c9a8fdc2836924343c6283a2a519450
Sesameboy <53367184+Sesameboy@users.noreply.github.com>
2025-01-19 20:28:43 +0800

    [MOD] 添加两个vector初始化示例，数组和string

--- MySTL/InitPattern/init_vector.hpp
+++ MySTL/InitPattern/init_vector.hpp
@@ -1,4 +1,5 @@
 #pragma  once
+#include <array>
 #include<iostream>
 #include<vector>
 
@@ -13,5 +14,17 @@ namespace init_pattern{
         std::vector<int> v7 = {1, 2, 3, 4, 5}; // vector with 5 elements with value 1, 2, 3, 4, 5
         std::cout << "v1: " << v1.size() << std::endl;
         std::cout << "v2: " << v2.size() << std::endl;
+
+        std::array<int, 8> a1{1}; // array with 8 elements, first element is 1, rest are 0
+        std::vector<int> v8(a1.begin() + 1, a1.begin() + 5); // vector with sequence of a1
+        std::cout << "v8: " << v8.size() << std::endl;// {1-5} the result is 4
+
+        int a2[] = {1, 2, 3, 4, 5};
+        std::vector<int> v9(std::begin(a2), std::end(a2)); // vector with elements of a2
+
+        // Initialize vector<char> from string
+        std::string str = "hello";
+        std::vector<char> v10(str.begin(), str.end()); // vector with characters of str
+        std::cout << "v10: " << v10.size() << std::endl;
     }
 }
